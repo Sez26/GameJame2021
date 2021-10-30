f21321b2f43f9e50a311571b03ccd1f6af9fc977
baconsenpaii <baconforevery2@gmail.com>
2021-10-30 13:40:52 +0100

    rename, look vector shenanigans

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5;
 
     private Vector3 movementVector = Vector3.zero;
+    private Vector3 lookVector = new Vector3(1, 0, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +19,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movementVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0).normalized
+        movementVector = Vector3.Normalize(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0))
             * speed * Time.deltaTime;
+
+        lookVector = Camera.main.ScreenToWorldPoint(Input.mousePosition) - this.transform.position;
+        lookVector.z = 0;
+        lookVector = Vector3.Normalize(lookVector);
 
         this.transform.position = this.transform.position + movementVector;
     }
 
     private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
         Gizmos.DrawRay(this.transform.position, movementVector * 120);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(this.transform.position, lookVector * 2);
     }
 }
