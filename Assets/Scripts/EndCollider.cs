41236dad7ddca31ba744ae0cf3b0a504d2ea3441
James-Millan <nf20792@bristol.ac.uk>
2021-10-31 10:59:11 +0000

    final

--- Assets/Scripts/EndCollider.cs
+++ Assets/Scripts/EndCollider.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class EndCollider : MonoBehaviour
 {
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
     }
     private void OnCollisionEnter2D(Collision2D other)  {
-        if(other.gameObject.tag == player)  {
-            Application.LoadLevel((Application.LoadedLevel + 1) % 5);
+        Debug.Log("yes");
+        if(other.gameObject.tag == "Player")  {
+            Application.LoadLevel((int)((Application.loadedLevel + 1) % 5));
         }
     }
 
