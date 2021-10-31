99124092c67613e5f1c7b6e890fbf5bdd99c43b7
Nathan Patel <30420600+baconsenpaii@users.noreply.github.com>
2021-10-31 04:06:30 +0000

    camera now moves slowly

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -6,18 +6,24 @@ public class CameraController : MonoBehaviour
 {
 
     public float cameraLockDistance = 4;
+    public float cameraMoveSpeed = 30;
+
+    private Vector3 prevPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        prevPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = this.transform.parent.GetComponent<PlayerController>().LookVector * cameraLockDistance
+        var target = this.transform.parent.GetComponent<PlayerController>().LookVector * cameraLockDistance
             + this.transform.parent.transform.position;
+        this.transform.Translate(new Vector3(0, 0, 10));
+        this.transform.position = Vector2.MoveTowards(prevPos, target, cameraLockDistance * cameraMoveSpeed * Time.deltaTime);
         this.transform.Translate(new Vector3(0, 0, -10));
+        prevPos = this.transform.position;
     }
 }
